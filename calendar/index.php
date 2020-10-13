1218ac51be7f5bdd2ab02522955661f644bc4ec4
Gus <gusta1296@gmail.com>
2020-10-12 23:28:06 -0700

    the calendar month switching and day clicking is working

--- calendar/index.php
+++ calendar/index.php
@@ -37,9 +37,7 @@ $db = db_connect();
 
 if( $_POST['action'] == 'get_datetime_slots' ){
 
-  $data = json_decode($_POST['user_datetime']);
-
-
+  $data = json_decode($_POST['the_date']);
 
   $sql = 'SELECT * FROM second_appointment ';
   $sql .= 'WHERE start_datetime > "' . $db->real_escape_string($data) . '"';
@@ -51,10 +49,111 @@ if( $_POST['action'] == 'get_datetime_slots' ){
 
 }
 
+function build_calendar($month,$year) {
 
+  // Create array containing abbreviations of days of week.
+  $daysOfWeek = array('S','M','T','W','T','F','S');
 
+  // What is the first day of the month in question?
+  $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
+
+  // How many days does this month contain?
+  $numberDays = date('t',$firstDayOfMonth);
+
+  // Retrieve some information about the first day of the
+  // month in question.
+  $dateComponents = getdate($firstDayOfMonth);
+
+  // What is the name of the month in question?
+  $monthName = $dateComponents['month'];
+
+  // What is the index value (0-6) of the first day of the
+  // month in question.
+  $dayOfWeek = $dateComponents['wday'];
+
+  // Create the table tag opener and day headers
+
+  $calendar = "<table class='calendar'>";
+  $calendar .= "<caption data-month-name='$monthName' data-year-int='$year'><a href='javascript:;' class='previous_month'><<</a> <span class='c_month'>$monthName</span> <span class='c_year'>$year</span> <a href='javascript:;' class='next_month'>>></a></caption>";
+  $calendar .= "<tr>";
+
+  // Create the calendar headers
+
+  foreach($daysOfWeek as $day) {
+    $calendar .= "<th class='header'>$day</th>";
+  } 
+
+  // Create the rest of the calendar
+
+  // Initiate the day counter, starting with the 1st.
+
+  $currentDay = 1;
+
+  $calendar .= "</tr><tr>";
+
+  // The variable $dayOfWeek is used to
+  // ensure that the calendar
+  // display consists of exactly 7 columns.
+
+  if ($dayOfWeek > 0) { 
+    $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>"; 
+  }
   
+  $month = str_pad($month, 2, "0", STR_PAD_LEFT);
 
+  while ($currentDay <= $numberDays) {
+
+    // Seventh column (Saturday) reached. Start a new row.
+
+    if ($dayOfWeek == 7) {
+
+      $dayOfWeek = 0;
+      $calendar .= "</tr><tr>";
+
+    }
+    
+    $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
+    
+    $date = "$year-$month-$currentDayRel";
+
+    $calendar .= "<td class='day'><a href='javascript:;' rel='$date'>$currentDay</a></td>";
+
+    // Increment counters
+
+    $currentDay++;
+    $dayOfWeek++;
+
+  }
+
+  // Complete the row of the last week in month, if necessary
+
+  if ($dayOfWeek != 7) { 
+  
+    $remainingDays = 7 - $dayOfWeek;
+    $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>"; 
+
+  }
+  
+  $calendar .= "</tr>";
+
+  $calendar .= "</table>";
+
+  return $calendar;
+
+}
+
+
+if($_POST['action'] == 'get_month'){
+
+  $the_date = json_decode($_POST['the_date']);
+
+  $month = date('m', strtotime( $the_date) ); 			     
+  $year = date('Y', strtotime( $the_date) );
+
+  $the_calendar = build_calendar($month, $year);
+
+  echo json_encode($the_calendar);
+}
 
 
 // $sql = 'SELECT * FROM second_appointment';
@@ -78,9 +177,5 @@ if( $_POST['action'] == 'get_datetime_slots' ){
 // echo $diff->format('%h hours %i minutes %s seconds').'<br>';
 
 // $result->free();
-
-
-
-
 
 ?>
